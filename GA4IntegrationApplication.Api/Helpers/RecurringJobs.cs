3cfd531cba24bd4e08bb09060999ac0e62427e7c
ertugrulakdag <ertugrulakdag@gmail.com>
2024-03-03 01:10:59 +0300

    hangfire was implemented

---
+++ GA4IntegrationApplication.Api/Helpers/RecurringJobs.cs
@@ -0,0 +1,40 @@
+﻿using GA4IntegrationApplication.Api.Interfaces;
+using Hangfire;
+using Hangfire.States;
+
+namespace GA4IntegrationApplication.Api.Helpers
+{
+    public static class BackgroundTasks
+    {
+        public static void GetJobs()
+        {
+            //Fire-And-Forget Jobs:İş tanımlanır ve hemen ardından bir kereye mahsus tetiklenir.
+            BackgroundJob.Enqueue(() => Console.WriteLine("Fire-And-Forget Jobs tetiklendi"));
+
+            //Recurring Jobs:Belirlenen CRON zamanlamasına göre tekrarlanan işler tanımlanır.
+            //Hangfire.RecurringJob.AddOrUpdate<IGoogleAnalytic>("RealTimeJob", x => x.Realtime(), Hangfire.Cron.MinuteInterval(1), TimeZoneInfo.FindSystemTimeZoneById("Turkey Standard Time")); //Her 1 dakkada bir çalışır
+            RecurringJob.AddOrUpdate("console-write-line", () => Console.WriteLine($"Hi:{DateTime.UtcNow}"), "*/10 * * * * *");//On Saniyede Bir Calişır
+            RecurringJob.AddOrUpdate<IGoogleAnalytic>("real-time-function", g => g.Realtime(), "*/15 * * * * *");
+
+
+            //Delayed Jobs:Oluşturulduktan belirli bir zaman sonra sadece bir seferliğine tetiklenecek olan görevler tanımlanır.
+            BackgroundJob.Schedule(() => Console.WriteLine("Delayed jobs tetiklendi!"), TimeSpan.FromSeconds(10));
+
+            //Continuations:Birbiriyle ilişkili işlerin olduğu durumlarda alınan aksiyondur. Bir jobun tetiklenebilmesi için bir öncekinin tamamlanması gerekmektedir.
+
+            var jobId = BackgroundJob.Schedule<IGoogleAnalytic>((g) => g.ScreenPageViews(DateTime.Now, DateTime.Now), TimeSpan.FromSeconds(10));
+            BackgroundJob.ContinueJobWith(jobId, () => Console.WriteLine($"ScreenPageViewsJobs isimli job {jobId} numarası ile {DateTime.Now} tarihinde çalıştı."));
+
+        }
+    }
+}
+//"00 0,1,7,8,13,14,18,20 * * *" //İlgili Saatlerde Çalışır
+//"*/30 * * * *" //Her 30 Dakikada Bir Çalışır
+//"*/10 * * * * *" //On Saniyede Bir Calişır
+//"1 * * * * *" //Bir Dakikada Bir Calişır
+//"00 12,18 * * *" //Her Gün Öglen OnIki ve Akşam OnSekiz Calişır
+//"00 07-18 * * *" //Sabah Yedi Akşam OnSekiz Arasında Her Saat Çalışır
+//"00 09-18 * * 1-5" //Hafta İçi Sabah Dokuz Akşam OnSekiz Arasinda Her Saat Çalışır
+//"*/10 * * * * *" //OnSaniyede Bir Çalişır
+//"0 9 * * 0" //Her Pazar Çalişır
+//"0 5 1 * *" //Her Ayın İlk Günü Saat Beşte Çaliş
--- GA4IntegrationApplication.Api/Program.cs
+++ GA4IntegrationApplication.Api/Program.cs
@@ -1,6 +1,13 @@
+using GA4IntegrationApplication.Api.Helpers;
 using GA4IntegrationApplication.Api.Infrastructure;
 using GA4IntegrationApplication.Api.Interfaces;
 using GA4IntegrationApplication.Api.Model;
+using Hangfire;
+using Hangfire.Dashboard;
+using Hangfire.MemoryStorage;
+using Hangfire.SqlServer;
+using HangfireBasicAuthenticationFilter;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +28,23 @@ builder.Services.AddSingleton<IGoogleAnalytic, GoogleAnalytic>();
 //AddTransient:Uygulama içerisinde bağımlılık olarak oluşturduğumuz ve kullandığımız nesnenin her kullanım ve çağrıda tekrardan oluşturulmasını sağlar.
 //AddSingleton:Uygulama içerisinde bağımlılık oluşturduğumuz ve kullandığımız nesnenin tek bir sefer oluşturulmasını ve aynı nesnenin uygulama içinde kullanılmasını sağlar.
 //AddScoped:Uygulama içerisindeki bağımlılık oluşturduğumu nesnenin request sonlanana kadar aynı nesneyi kullanmasını farklı bir çağrı için gelindiğinde yeni bir nesne yaratılmasını sağlar.
+builder.Services.AddHangfire(h =>
+                   h.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                    .UseDashboardMetric(DashboardMetrics.ServerCount)
+                    .UseDashboardMetric(DashboardMetrics.RecurringJobCount)
+                    .UseDashboardMetric(DashboardMetrics.RetriesCount)
+                    .UseDashboardMetric(DashboardMetrics.AwaitingCount)
+                    .UseDashboardMetric(DashboardMetrics.EnqueuedAndQueueCount)
+                    .UseDashboardMetric(DashboardMetrics.ScheduledCount)
+                    .UseDashboardMetric(DashboardMetrics.ProcessingCount)
+                    .UseDashboardMetric(DashboardMetrics.SucceededCount)
+                    .UseDashboardMetric(DashboardMetrics.FailedCount)
+                    .UseDashboardMetric(DashboardMetrics.DeletedCount)
+                    .UseSimpleAssemblyNameTypeSerializer()
+                    .UseDefaultTypeSerializer()
+                    .UseMemoryStorage());
+builder.Services.AddHangfireServer();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -33,6 +57,21 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseHangfireDashboard("/job", new DashboardOptions
+{
+    Authorization = new[]
+{
+    new HangfireCustomBasicAuthenticationFilter
+    {
+         User = builder.Configuration.GetSection("HangfireSettings:UserName").Value,
+         Pass = builder.Configuration.GetSection("HangfireSettings:Password").Value
+    }
+    }
+});
+GlobalJobFilters.Filters.Add(new AutomaticRetryAttribute { Attempts = 3 });// ilgili job başarılı şekilde çalıştırılamaz ise, hata alınılmayıncaya kadar 3 defa tekrar edilmektedir.
+BackgroundTasks.GetJobs();
+
 
 app.MapControllers();
 
